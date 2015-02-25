025b50d552edec651fdf5d1ce13c7028b9bdfe7a
Jiang li <322137@qq.com>
2015-02-25 19:43:22 +0800

    create project

---
+++ PlaceholderTextView/PlaceholderTextView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PlaceholderTextView
+//
+//  Created by Aimy on 15/2/25.
+//  Copyright (c) 2015年 Aimy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
